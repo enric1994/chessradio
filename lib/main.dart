0d999fcf3c5e087e64eafd27a87039b5f8ea1c9b
Enric Moreu <enric.moreu.filella@gmail.com>
2021-05-16 02:10:32 +0200

    Carousel + sliders

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,21 @@
 import 'package:flutter/material.dart';
+// import 'package:carousel_slider/carousel_slider.dart';
+
+import './voiceSelector.dart';
 
 void main() {
   runApp(ChessRadio());
 }
 
-class ChessRadio extends StatelessWidget {
+class ChessRadio extends StatefulWidget {
+  @override
+  _ChessRadioState createState() => _ChessRadioState();
+}
+
+class _ChessRadioState extends State<ChessRadio> {
+  double _checkMateValue = 1;
+  double _speedValue = 1;
+  double _piecesValue = 8;
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
@@ -13,11 +24,59 @@ class ChessRadio extends StatelessWidget {
           title: Text('Chess Radio'),
           backgroundColor: const Color(0xfff4dd29),
         ),
-        body: Column(
-          children: [
-            FloatingActionButton(
-                child: const Icon(Icons.play_arrow), onPressed: null),
-          ],
+        body: Center(
+          child: new ListView(
+              shrinkWrap: true,
+              padding: const EdgeInsets.all(20.0),
+              children: [
+                Row(
+                  children: [
+                    Text('Number of pieces: '),
+                    Text(
+                      _piecesValue.round().toString(),
+                      style: TextStyle(fontWeight: FontWeight.bold),
+                    ),
+                  ],
+                ),
+                Slider(
+                  value: _piecesValue,
+                  min: 4,
+                  max: 20,
+                  divisions: 16,
+                  label: _piecesValue.round().toString() + ' pieces',
+                  onChanged: (value) {
+                    setState(() {
+                      _piecesValue = value;
+                    });
+                  },
+                ),
+                Row(
+                  children: [
+                    Text('Mate in: '),
+                    Text(_checkMateValue.round().toString(),
+                        style: TextStyle(fontWeight: FontWeight.bold)),
+                  ],
+                ),
+                Slider(
+                  value: _checkMateValue,
+                  min: 1,
+                  max: 4,
+                  divisions: 3,
+                  label: 'M' + _checkMateValue.round().toString(),
+                  onChanged: (value) {
+                    setState(() {
+                      _checkMateValue = value;
+                    });
+                  },
+                ),
+                VoiceSelector(),
+                Center(
+                  child: ElevatedButton(
+                    child: Text('Start!'),
+                    onPressed: null,
+                  ),
+                )
+              ]),
         ),
       ),
     );
---
+++ lib/voiceSelector.dart
@@ -0,0 +1,61 @@
+import 'package:flutter/material.dart';
+import 'package:carousel_slider/carousel_controller.dart';
+import 'package:carousel_slider/carousel_options.dart';
+import 'package:carousel_slider/carousel_slider.dart';
+
+final List<String> imgList = [
+  'images/CodeMiko.png',
+  'images/Cramling.png',
+  'images/GothamChess.png',
+  'images/Hikaru.png',
+  'images/Kosteniuk.png',
+  'images/Vishy.png',
+];
+
+class VoiceSelector extends StatefulWidget {
+  @override
+  _VoiceSelectorState createState() => _VoiceSelectorState();
+}
+
+class _VoiceSelectorState extends State<VoiceSelector> {
+  var _currentVoice;
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: new ListView(
+        shrinkWrap: true,
+        padding: const EdgeInsets.all(20.0),
+        children: [
+          Row(
+            children: [
+              Text('Voice: '),
+              Text(
+                _currentVoice.toString(),
+                style: TextStyle(fontWeight: FontWeight.bold),
+              ),
+            ],
+          ),
+          CarouselSlider(
+            options: CarouselOptions(
+                enlargeCenterPage: true,
+                viewportFraction: 0.5,
+                onPageChanged: (index, reason) {
+                  setState(() {
+                    var str = imgList[index].split('/')[1];
+                    if (str != null && str.length >= 4) {
+                      str = str.substring(0, str.length - 4);
+                    }
+                    _currentVoice = str;
+                  });
+                }),
+            items: imgList
+                .map((item) => Container(
+                      child: Center(child: Image(image: AssetImage(item))),
+                    ))
+                .toList(),
+          )
+        ],
+      ),
+    );
+  }
+}
