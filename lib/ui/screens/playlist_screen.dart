35a9cee6727a4cbeb28664ee038537af15ff953b
Oriol Bernal <obernalpo@gmail.com>
2021-06-07 22:11:01 +0200

    try just_audio with custom playlist

--- lib/ui/screens/playlist_screen.dart
+++ lib/ui/screens/playlist_screen.dart
@@ -1,48 +1,104 @@
-import 'package:chessradio/ui/screens/selector_screen.dart';
 import 'package:flutter/material.dart';
+import 'package:just_audio/just_audio.dart';
+import 'package:audio_session/audio_session.dart';
+import 'package:flutter/services.dart';
+import 'package:rxdart/rxdart.dart';
+import 'dart:math';
 
-import '../widgets/chess_radio_title_widget.dart';
-import '../widgets/puzzle_widget.dart';
-import '../widgets/chess_radio_drawer_widget.dart';
+class PlayListScreen extends StatefulWidget {
+  @override
+  _PlayListScreenState createState() => _PlayListScreenState();
+}
 
-class PlayListScreen extends StatelessWidget {
-  final audios = <String>[
-    "assets/audio/test.wav",
-    "assets/audio/test.wav",
-    "assets/audio/test.wav",
-  ];
-  PlayListScreen();
+class _PlayListScreenState extends State<PlayListScreen> {
+  late AudioPlayer _player;
+
+  final _playlist = ConcatenatingAudioSource(children: [
+    AudioSource.uri(
+      Uri.parse("assets/puzzles/1_challenge_female1.mp3"),
+    ),
+    AudioSource.uri(
+      Uri.parse("assets/puzzles/2_challenge_female2.mp3"),
+    ),
+    AudioSource.uri(
+      Uri.parse("assets/puzzles/3_challenge_male1.mp3"),
+    ),
+    AudioSource.uri(
+      Uri.parse("assets/puzzles/4_challenge_male2.mp3"),
+    ),
+  ]);
+
+  @override
+  void initState() {
+    super.initState();
+    _player = AudioPlayer();
+    SystemChrome.setSystemUIOverlayStyle(SystemUiOverlayStyle(
+      statusBarColor: Colors.black,
+    ));
+    _init();
+  }
+
+  Future<void> _init() async {
+    final session = await AudioSession.instance;
+    await session.configure(AudioSessionConfiguration.speech());
+    // Listen to errors during playback.
+    _player.playbackEventStream.listen((event) {},
+        onError: (Object e, StackTrace stackTrace) {
+      print('A stream error occurred: $e');
+    });
+    try {
+      await _player.setAudioSource(_playlist);
+    } catch (e) {
+      // Catch load errors: 404, invalid url ...
+      print("Error loading playlist: $e");
+    }
+  }
+
+  @override
+  void dispose() {
+    _player.dispose();
+    super.dispose();
+  }
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: Hero(
-        tag: 'playlist',
-        child: Scaffold(
-          appBar: AppBar(
-            title: ChessRadioTitleWidget(),
-            backgroundColor: Colors.black,
-            actions: [
-              ChessRadioDrawerWidget(),
-            ],
-            leading: IconButton(
-              icon: Icon(Icons.arrow_back),
-              // color: Colors.white,
-              onPressed: () {
-                Navigator.of(context).push(
-                  MaterialPageRoute(
-                    builder: (context) => SelectorScreen(),
+      debugShowCheckedModeBanner: false,
+      home: Scaffold(
+        body: SafeArea(
+          child: Center(
+            child: ListView.builder(
+              itemCount: _playlist.length,
+              itemBuilder: (context, index) {
+                return Container(
+                  child: Row(
+                    children: [
+                      Container(
+                        child: Track(_playlist[index]),
+                        width: 350,
+                        height: 100,
+                        margin: EdgeInsets.all(20),
+                        decoration: BoxDecoration(
+                          borderRadius: BorderRadius.circular(15.0),
+                          color: Colors.white,
+                          border: Border.all(
+                            color: Colors.black,
+                            width: 4,
+                          ),
+                        ),
+                      ),
+                      Container(
+                        width: 70.0,
+                        decoration: BoxDecoration(),
+                        child: Image(
+                          image: AssetImage('./images/solution.png'),
+                          height: 70,
+                          alignment: Alignment.bottomCenter,
+                        ),
+                      ),
+                    ],
                   ),
                 );
-              },
-            ),
-          ),
-          backgroundColor: Colors.white,
-          body: Center(
-            child: ListView.builder(
-              itemCount: audios.length,
-              itemBuilder: (context, index) {
-                return PuzzleWidget();
               },
             ),
           ),
@@ -50,4 +106,242 @@ class PlayListScreen extends StatelessWidget {
       ),
     );
   }
+}
+
+class Track extends StatefulWidget {
+  final AudioPlayer _player = AudioPlayer();
+  final AudioSource source;
+
+  Track(this.source);
+
+  @override
+  _TrackState createState() => _TrackState();
+}
+
+class _TrackState extends State<Track> {
+  @override
+  void initState() {
+    super.initState();
+    SystemChrome.setSystemUIOverlayStyle(SystemUiOverlayStyle(
+      statusBarColor: Colors.black,
+    ));
+    _init();
+  }
+
+  Future<void> _init() async {
+    final session = await AudioSession.instance;
+    await session.configure(AudioSessionConfiguration.speech());
+    // Listen to errors during playback.
+    widget._player.playbackEventStream.listen((event) {},
+        onError: (Object e, StackTrace stackTrace) {
+      print('A stream error occurred: $e');
+    });
+    try {
+      await widget._player.setAudioSource(widget.source);
+    } catch (e) {
+      // Catch load errors: 404, invalid url ...
+      print("Error loading playlist: $e");
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      mainAxisSize: MainAxisSize.min,
+      children: [
+        StreamBuilder<PlayerState>(
+          stream: widget._player.playerStateStream,
+          builder: (context, snapshot) {
+            final playerState = snapshot.data;
+            final processingState = playerState?.processingState;
+            final playing = playerState?.playing;
+            if (processingState == ProcessingState.loading ||
+                processingState == ProcessingState.buffering) {
+              return Container(
+                margin: EdgeInsets.all(8.0),
+                width: 64.0,
+                height: 64.0,
+                child: CircularProgressIndicator(),
+              );
+            } else if (playing != true) {
+              return IconButton(
+                icon: Icon(Icons.play_arrow),
+                iconSize: 64.0,
+                onPressed: widget._player.play,
+              );
+            } else if (processingState != ProcessingState.completed) {
+              return IconButton(
+                icon: Icon(Icons.pause),
+                iconSize: 64.0,
+                onPressed: widget._player.pause,
+              );
+            } else {
+              return IconButton(
+                icon: Icon(Icons.replay),
+                iconSize: 64.0,
+                onPressed: () => widget._player.seek(Duration.zero,
+                    index: widget._player.effectiveIndices!.first),
+              );
+            }
+          },
+        ),
+        StreamBuilder<Duration?>(
+          stream: widget._player.durationStream,
+          builder: (context, snapshot) {
+            final duration = snapshot.data ?? Duration.zero;
+            return StreamBuilder<PositionData>(
+              stream: Rx.combineLatest2<Duration, Duration, PositionData>(
+                  widget._player.positionStream,
+                  widget._player.bufferedPositionStream,
+                  (position, bufferedPosition) =>
+                      PositionData(position, bufferedPosition)),
+              builder: (context, snapshot) {
+                final positionData =
+                    snapshot.data ?? PositionData(Duration.zero, Duration.zero);
+                var position = positionData.position;
+                if (position > duration) {
+                  position = duration;
+                }
+                var bufferedPosition = positionData.bufferedPosition;
+                if (bufferedPosition > duration) {
+                  bufferedPosition = duration;
+                }
+                return SeekBar(
+                  duration: duration,
+                  position: position,
+                  bufferedPosition: bufferedPosition,
+                  onChangeEnd: (newPosition) {
+                    widget._player.seek(newPosition);
+                  },
+                );
+              },
+            );
+          },
+        ),
+      ],
+    );
+  }
+}
+
+class PositionData {
+  final Duration position;
+  final Duration bufferedPosition;
+
+  PositionData(this.position, this.bufferedPosition);
+}
+
+class SeekBar extends StatefulWidget {
+  final Duration duration;
+  final Duration position;
+  final Duration bufferedPosition;
+  final ValueChanged<Duration>? onChanged;
+  final ValueChanged<Duration>? onChangeEnd;
+
+  SeekBar({
+    required this.duration,
+    required this.position,
+    required this.bufferedPosition,
+    this.onChanged,
+    this.onChangeEnd,
+  });
+
+  @override
+  _SeekBarState createState() => _SeekBarState();
+}
+
+class _SeekBarState extends State<SeekBar> {
+  double? _dragValue;
+  late SliderThemeData _sliderThemeData;
+
+  @override
+  void didChangeDependencies() {
+    super.didChangeDependencies();
+
+    _sliderThemeData = SliderTheme.of(context).copyWith(
+      trackHeight: 2.0,
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Stack(
+      children: [
+        SliderTheme(
+          data: _sliderThemeData.copyWith(
+            activeTrackColor: Colors.blue.shade100,
+            inactiveTrackColor: Colors.grey.shade300,
+          ),
+          child: ExcludeSemantics(
+            child: Slider(
+              min: 0.0,
+              max: widget.duration.inMilliseconds.toDouble(),
+              value: widget.bufferedPosition.inMilliseconds.toDouble(),
+              onChanged: (value) {
+                setState(() {
+                  _dragValue = value;
+                });
+                if (widget.onChanged != null) {
+                  widget.onChanged!(Duration(milliseconds: value.round()));
+                }
+              },
+              onChangeEnd: (value) {
+                if (widget.onChangeEnd != null) {
+                  widget.onChangeEnd!(Duration(milliseconds: value.round()));
+                }
+                _dragValue = null;
+              },
+            ),
+          ),
+        ),
+        SliderTheme(
+          data: _sliderThemeData.copyWith(
+            inactiveTrackColor: Colors.transparent,
+          ),
+          child: Slider(
+            min: 0.0,
+            max: widget.duration.inMilliseconds.toDouble(),
+            value: min(_dragValue ?? widget.position.inMilliseconds.toDouble(),
+                widget.duration.inMilliseconds.toDouble()),
+            onChanged: (value) {
+              setState(() {
+                _dragValue = value;
+              });
+              if (widget.onChanged != null) {
+                widget.onChanged!(Duration(milliseconds: value.round()));
+              }
+            },
+            onChangeEnd: (value) {
+              if (widget.onChangeEnd != null) {
+                widget.onChangeEnd!(Duration(milliseconds: value.round()));
+              }
+              _dragValue = null;
+            },
+          ),
+        ),
+      ],
+    );
+  }
+
+  Duration get _remaining => widget.duration - widget.position;
+}
+
+class HiddenThumbComponentShape extends SliderComponentShape {
+  @override
+  Size getPreferredSize(bool isEnabled, bool isDiscrete) => Size.zero;
+
+  @override
+  void paint(
+    PaintingContext context,
+    Offset center, {
+    required Animation<double> activationAnimation,
+    required Animation<double> enableAnimation,
+    required bool isDiscrete,
+    required TextPainter labelPainter,
+    required RenderBox parentBox,
+    required SliderThemeData sliderTheme,
+    required TextDirection textDirection,
+    required double value,
+    required double textScaleFactor,
+    required Size sizeWithOverflow,
+  }) {}
 }
