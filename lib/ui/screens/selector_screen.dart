47d40c6f0ccf4f01d4b75ad60be368d6b251bd47
Enric Moreu <enric.moreu.filella@gmail.com>
2021-06-07 19:35:51 +0200

    Selector screen difficulty

--- lib/ui/screens/selector_screen.dart
+++ lib/ui/screens/selector_screen.dart
@@ -1,5 +1,4 @@
-import 'package:chessradio/ui/widgets/check_mate_value_widget.dart';
-import 'package:chessradio/ui/widgets/pieces_number_widget.dart';
+import 'package:chessradio/ui/widgets/difficulty_widget.dart';
 import 'package:chessradio/ui/widgets/start_button_widget.dart';
 import 'package:chessradio/ui/widgets/voice_selector_widget.dart';
 import 'package:flutter/material.dart';
@@ -24,8 +23,9 @@ class SelectorScreen extends StatelessWidget {
         body: Center(
           child: Column(
             children: [
-              PiecesNumberWidget(),
-              CheckMateValueWidget(),
+              // PiecesNumberWidget(),
+              DifficultyWidget(),
+              // CheckMateValueWidget(),
               VoiceSelectorWidget(),
               StartButtonWidget(),
             ],
