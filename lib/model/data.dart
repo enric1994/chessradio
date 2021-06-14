7b73e1d4d3103da658c1c8d33d84f1a81e1e7f80
oriolbernal <obernalpo@gmail.com>
2021-06-14 19:24:40 +0200

    build test with href="build/web/"

--- lib/model/data.dart
+++ lib/model/data.dart
@@ -3,43 +3,75 @@ import 'package:chessradio/model/voice.dart';
 
 final List<Voice> voices = [
   Voice('Hikaru Nakamura', 'assets/images/Hikaru Nakamura.jpeg', [
-    Puzzle('assets/puzzles/1_challenge_male1.mp3',
-        'assets/puzzles/1_solution_male1.mp3', 'assets/puzzles/1.jpeg'),
-    Puzzle('assets/puzzles/2_challenge_male1.mp3',
-        'assets/puzzles/2_solution_male1.mp3', 'assets/puzzles/2.jpeg'),
-    Puzzle('assets/puzzles/3_challenge_male1.mp3',
-        'assets/puzzles/3_solution_male1.mp3', 'assets/puzzles/3.jpeg'),
-    Puzzle('assets/puzzles/4_challenge_male1.mp3',
-        'assets/puzzles/4_solution_male1.mp3', 'assets/puzzles/4.jpeg'),
+    Puzzle(
+        'https://github.com/enric1994/chessradio/raw/master/assets/puzzles/1_challenge_male1.mp3',
+        'https://github.com/enric1994/chessradio/raw/master/assets/puzzles/1_solution_male1.mp3',
+        'assets/puzzles/1.jpeg'),
+    Puzzle(
+        'https://github.com/enric1994/chessradio/raw/master/assets/puzzles/2_challenge_male1.mp3',
+        'https://github.com/enric1994/chessradio/raw/master/assets/puzzles/2_solution_male1.mp3',
+        'assets/puzzles/2.jpeg'),
+    Puzzle(
+        'https://github.com/enric1994/chessradio/raw/master/assets/puzzles/3_challenge_male1.mp3',
+        'https://github.com/enric1994/chessradio/raw/master/assets/puzzles/3_solution_male1.mp3',
+        'assets/puzzles/3.jpeg'),
+    Puzzle(
+        'https://github.com/enric1994/chessradio/raw/master/assets/puzzles/4_challenge_male1.mp3',
+        'https://github.com/enric1994/chessradio/raw/master/assets/puzzles/4_solution_male1.mp3',
+        'assets/puzzles/4.jpeg'),
   ]),
   Voice('Anna Rudolf', 'assets/images/Anna Rudolf.jpeg', [
-    Puzzle('assets/puzzles/1_challenge_female1.mp3',
-        'assets/puzzles/1_solution_female1.mp3', 'assets/puzzles/1.jpeg'),
-    Puzzle('assets/puzzles/2_challenge_female1.mp3',
-        'assets/puzzles/2_solution_female1.mp3', 'assets/puzzles/2.jpeg'),
-    Puzzle('assets/puzzles/3_challenge_female1.mp3',
-        'assets/puzzles/3_solution_female1.mp3', 'assets/puzzles/3.jpeg'),
-    Puzzle('assets/puzzles/4_challenge_female1.mp3',
-        'assets/puzzles/4_solution_female1.mp3', 'assets/puzzles/4.jpeg'),
+    Puzzle(
+        'https://github.com/enric1994/chessradio/raw/master/assets/puzzles/1_challenge_female1.mp3',
+        'https://github.com/enric1994/chessradio/raw/master/assets/puzzles/1_solution_female1.mp3',
+        'assets/puzzles/1.jpeg'),
+    Puzzle(
+        'https://github.com/enric1994/chessradio/raw/master/assets/puzzles/2_challenge_female1.mp3',
+        'https://github.com/enric1994/chessradio/raw/master/assets/puzzles/2_solution_female1.mp3',
+        'assets/puzzles/2.jpeg'),
+    Puzzle(
+        'https://github.com/enric1994/chessradio/raw/master/assets/puzzles/3_challenge_female1.mp3',
+        'https://github.com/enric1994/chessradio/raw/master/assets/puzzles/3_solution_female1.mp3',
+        'assets/puzzles/3.jpeg'),
+    Puzzle(
+        'https://github.com/enric1994/chessradio/raw/master/assets/puzzles/4_challenge_female1.mp3',
+        'https://github.com/enric1994/chessradio/raw/master/assets/puzzles/4_solution_female1.mp3',
+        'assets/puzzles/4.jpeg'),
   ]),
   Voice('Lile Koridze', 'assets/images/Lile Koridze.jpeg', [
-    Puzzle('assets/puzzles/1_challenge_female2.mp3',
-        'assets/puzzles/1_solution_female2.mp3', 'assets/puzzles/1.jpeg'),
-    Puzzle('assets/puzzles/2_challenge_female2.mp3',
-        'assets/puzzles/2_solution_female2.mp3', 'assets/puzzles/2.jpeg'),
-    Puzzle('assets/puzzles/3_challenge_female2.mp3',
-        'assets/puzzles/3_solution_female2.mp3', 'assets/puzzles/3.jpeg'),
-    Puzzle('assets/puzzles/4_challenge_female2.mp3',
-        'assets/puzzles/4_solution_female2.mp3', 'assets/puzzles/4.jpeg'),
+    Puzzle(
+        'https://github.com/enric1994/chessradio/raw/master/assets/puzzles/1_challenge_female2.mp3',
+        'https://github.com/enric1994/chessradio/raw/master/assets/puzzles/1_solution_female2.mp3',
+        'assets/puzzles/1.jpeg'),
+    Puzzle(
+        'https://github.com/enric1994/chessradio/raw/master/assets/puzzles/2_challenge_female2.mp3',
+        'https://github.com/enric1994/chessradio/raw/master/assets/puzzles/2_solution_female2.mp3',
+        'assets/puzzles/2.jpeg'),
+    Puzzle(
+        'https://github.com/enric1994/chessradio/raw/master/assets/puzzles/3_challenge_female2.mp3',
+        'https://github.com/enric1994/chessradio/raw/master/assets/puzzles/3_solution_female2.mp3',
+        'assets/puzzles/3.jpeg'),
+    Puzzle(
+        'https://github.com/enric1994/chessradio/raw/master/assets/puzzles/4_challenge_female2.mp3',
+        'https://github.com/enric1994/chessradio/raw/master/assets/puzzles/4_solution_female2.mp3',
+        'assets/puzzles/4.jpeg'),
   ]),
   Voice('Voyboy', 'assets/images/Voyboy.jpeg', [
-    Puzzle('assets/puzzles/1_challenge_male2.mp3',
-        'assets/puzzles/1_solution_male2.mp3', 'assets/puzzles/1.jpeg'),
-    Puzzle('assets/puzzles/2_challenge_male2.mp3',
-        'assets/puzzles/2_solution_male2.mp3', 'assets/puzzles/2.jpeg'),
-    Puzzle('assets/puzzles/3_challenge_male2.mp3',
-        'assets/puzzles/3_solution_male2.mp3', 'assets/puzzles/3.jpeg'),
-    Puzzle('assets/puzzles/4_challenge_male2.mp3',
-        'assets/puzzles/4_solution_male2.mp3', 'assets/puzzles/4.jpeg'),
+    Puzzle(
+        'https://github.com/enric1994/chessradio/raw/master/assets/puzzles/1_challenge_male2.mp3',
+        'https://github.com/enric1994/chessradio/raw/master/assets/puzzles/1_solution_male2.mp3',
+        'assets/puzzles/1.jpeg'),
+    Puzzle(
+        'https://github.com/enric1994/chessradio/raw/master/assets/puzzles/2_challenge_male2.mp3',
+        'https://github.com/enric1994/chessradio/raw/master/assets/puzzles/2_solution_male2.mp3',
+        'assets/puzzles/2.jpeg'),
+    Puzzle(
+        'https://github.com/enric1994/chessradio/raw/master/assets/puzzles/3_challenge_male2.mp3',
+        'https://github.com/enric1994/chessradio/raw/master/assets/puzzles/3_solution_male2.mp3',
+        'assets/puzzles/3.jpeg'),
+    Puzzle(
+        'https://github.com/enric1994/chessradio/raw/master/assets/puzzles/4_challenge_male2.mp3',
+        'https://github.com/enric1994/chessradio/raw/master/assets/puzzles/4_solution_male2.mp3',
+        'assets/puzzles/4.jpeg'),
   ]),
 ];
